e043efd6f9e576ac9c3a0cf1987ac9590616ccf2
dishajain-code <dishaj1908@gmail.com>
2025-07-05 16:02:07 +0530

    change 4

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,7 +1,24 @@
-const InterviewPage = () => {
-  return (
-    <div>InterviewPage</div>
-  )
-}
+import { getAssessments } from "@/actions/interview";
+import QuizList from "./_component/quiz-list";
+import PerformanceChart from "./_component/performace-chart";
+import StatsCards from "./_component/stats-cards";
 
-export default InterviewPage
+
+export default async function InterviewPrepPage() {
+  const assessments = await getAssessments();
+
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className=" mt-[80px] text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
+}
--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,9 +1,16 @@
-import React from 'react'
 
-const MainLayout = ({children}) => {
+import { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
+export default function Layout({ children }) {
   return (
-    <div className='container mx-auto mt-24 mb-20'>{children}</div>
-  )
+    <div className="px-5">
+      
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
 }
-
-export default MainLayout
