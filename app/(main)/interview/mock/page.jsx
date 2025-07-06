99542004d53eeaaa8011e7f1acf8062f724d53d5
dishajain-code <dishaj1908@gmail.com>
2025-07-06 14:47:36 +0530

    Update page.jsx

--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -7,7 +7,7 @@ import Quiz from "../_component/quiz";
 export default function MockInterviewPage() {
   return (
     <div className="container mx-auto space-y-4 py-6">
-      <div className="flex flex-col space-y-2 mx-2">
+      <div className="flex flex-col space-y-2 mx-2 mt-[100px]">
         <Link href="/interview">
           <Button variant="link" className="gap-2 pl-0">
             <ArrowLeft className="h-4 w-4" />
